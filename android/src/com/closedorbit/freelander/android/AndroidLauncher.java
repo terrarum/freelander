5d0ca736ceffb0d0e09926ed0b6ca35863e83867
terrarum <terrarum@gmail.com>
2014-04-26 19:02:19 +0100

    Initial commit, test project builds and deploys to desktop and android.

---
+++ android/src/com/closedorbit/freelander/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.closedorbit.freelander.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.closedorbit.freelander.Freelander;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Freelander(), config);
+	}
+}
