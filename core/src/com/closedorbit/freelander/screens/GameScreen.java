77cfd1707696cda3663e1d44a511b9be6c5a1d08
terrarum <terrarum@gmail.com>
2014-04-29 16:04:16 +0100

    Created entities with positions that apply to box2d and their sprites. Case 188

--- core/src/com/closedorbit/freelander/screens/GameScreen.java
+++ core/src/com/closedorbit/freelander/screens/GameScreen.java
@@ -4,11 +4,7 @@ import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Batch;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -16,37 +12,36 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.closedorbit.freelander.FontBuilder;
+import com.closedorbit.freelander.entities.RectangleEntity;
+import com.closedorbit.freelander.entities.ShipEntity;
+import com.closedorbit.freelander.factories.RectangleFactory;
+import com.closedorbit.freelander.factories.ShipFactory;
 import com.closedorbit.freelander.levelPackLoader.Level;
+import com.closedorbit.freelander.utilities.BoundedCamera;
 
 import static com.closedorbit.freelander.utilities.B2DVars.PPM;
 
 public class GameScreen extends DefaultScreen {
 
-    Batch batch;
+    SpriteBatch sb;
     Level levelData;
     World world;
-    OrthographicCamera camera;
-    OrthographicCamera b2dCam;
+
+    BoundedCamera b2dCam;
     Box2DDebugRenderer debugRenderer;
 
-    BodyDef shipDef;
-    Body shipBody;
-    PolygonShape shipShape;
-    FixtureDef shipFixtureDef;
-    Fixture shipFixture;
-    Texture shipTexture;
-    Sprite shipSprite;
-
-    Texture groundTexture;
-    Sprite groundSprite;
+    BoundedCamera cam;
+    OrthographicCamera hudCam;
 
     // HUD
-    OrthographicCamera hudCam;
     Skin skin;
     FontBuilder fontBuilder;
     Stage stage;
     Label hud1;
     Label hud2;
+
+    ShipEntity player;
+    RectangleEntity marker1;
     
     float V_WIDTH = 480;
     float V_HEIGHT = 800;
@@ -54,68 +49,41 @@ public class GameScreen extends DefaultScreen {
     public GameScreen(Game game, Level levelData) {
         super(game);
         this.levelData = levelData;
-        camera = new OrthographicCamera();
-        b2dCam = new OrthographicCamera();
-        camera.setToOrtho(false, V_WIDTH, V_HEIGHT); // Screen V_WIDTH and V_HEIGHT
+        cam = new BoundedCamera();
+        cam.setToOrtho(false, V_WIDTH, V_HEIGHT); // Screen V_WIDTH and V_HEIGHT
+        cam.setBounds(-800, 800, -500, 2000);
+
+        hudCam = new OrthographicCamera();
+        hudCam.setToOrtho(false, V_WIDTH, V_HEIGHT);
+
+        b2dCam = new BoundedCamera();
         b2dCam.setToOrtho(false, V_WIDTH / PPM, V_HEIGHT / PPM);
-    }
+        b2dCam.setBounds(-800 / PPM, 800 / PPM, -500 / PPM, 2000 / PPM);
 
-    public Body createBox(float x, float y, float w, float h) {
-        BodyDef boxDef = new BodyDef();
-        boxDef.position.set(new Vector2(x / PPM, y / PPM));
-        Body boxBody = world.createBody(boxDef);
-        PolygonShape boxShape = new PolygonShape();
-        boxShape.setAsBox(w / 2 / PPM, h / 2 / PPM);
-        boxBody.createFixture(boxShape, 0.0f);
-
-        return boxBody;
+        world = new World(levelData.gravity, true);
     }
 
     @Override
     public void show() {
-        world = new World(levelData.gravity, true);
         debugRenderer = new Box2DDebugRenderer();
 
-        //Ground body.
-        createBox(1, 0 - V_HEIGHT / 4, V_WIDTH * 4, V_HEIGHT / 2);
+        RectangleFactory rectFact = new RectangleFactory(world);
+        ShipFactory shipFact = new ShipFactory(world);
 
-        // Markers.
-        createBox(-30, 0,    10, 10);
-        createBox(-30, 200,  10, 10);
-        createBox(-30, 400,  10, 10);
-        createBox(-30, 600,  10, 10);
-        createBox(-30, 800,  10, 10);
-        createBox(-30, 1000, 10, 10);
+        player = shipFact.createShip(levelData, 0, 0, "images/dropship.png");
 
-        shipTexture = new Texture(Gdx.files.internal("images/dropship.png")); // Probably not using the atlas.
-        shipSprite = new Sprite(shipTexture, 0, 0, 24, 45);
+        rectFact.createRectangleEntity(1, 0 - V_HEIGHT / 4, V_WIDTH * 4, V_HEIGHT / 2, null);
 
-        groundTexture = new Texture(Gdx.files.internal("images/groundTile.png"));
-        groundTexture.setWrap(Texture.TextureWrap.Repeat, Texture.TextureWrap.Repeat);
+        marker1 = rectFact.createRectangleEntity(-30, 0, 10, 10, "images/marker.png");
 
-        shipDef = new BodyDef();
-        shipDef.type = BodyDef.BodyType.DynamicBody;
-        shipDef.position.set(levelData.ship.x / PPM, (levelData.ship.y + shipSprite.getHeight() / 2) / PPM );
+//        cam.position.set(player.getPosition().x, player.getPosition().y - 100, 0);
+        cam.setPosition(0, 0);
+        cam.update();
+        b2dCam.setPosition(0 / PPM, 0 / PPM);
+        b2dCam.update();
 
-        shipBody = world.createBody(shipDef);
-        shipBody.setLinearVelocity(levelData.shipStartingVelocity.x / PPM, levelData.shipStartingVelocity.y / PPM);
-        shipShape = new PolygonShape();
-        shipShape.setAsBox(24 / 2 / PPM, 45 / 2 / PPM);
-
-        shipFixtureDef = new FixtureDef();
-        shipFixtureDef.shape = shipShape;
-        shipFixtureDef.density = 1.0f;
-        shipFixtureDef.friction = 0.5f;
-        shipFixtureDef.restitution = 0f;
-
-        shipFixture = shipBody.createFixture(shipFixtureDef);
-        shipShape.dispose();
-
-        camera.position.set(shipDef.position.x, shipDef.position.y - 100, 0);
-        camera.update();
-
-        batch = new SpriteBatch();
-        batch.getProjectionMatrix().setToOrtho2D(0, 0, V_WIDTH, V_HEIGHT);
+        sb = new SpriteBatch();
+        sb.getProjectionMatrix().setToOrtho2D(0, 0, V_WIDTH, V_HEIGHT);
 
         // HUD.
         stage = new Stage();
@@ -139,34 +107,39 @@ public class GameScreen extends DefaultScreen {
         Gdx.gl.glClearColor(0, 0, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        // Input.
         if (Gdx.input.isTouched()) {
             Vector2 force = new Vector2(0, 400);
-            shipBody.applyForce(force, shipBody.getWorldCenter(), true);
+            player.body.applyForce(force, player.body.getWorldCenter(), true);
         }
 
-        shipSprite.setPosition(shipBody.getPosition().x - 24 / 2, shipBody.getPosition().y - 45 / 2);
+        // Camera follow player.
+//        cam.setPosition(player.getPosition().x * PPM + V_WIDTH / 4, player.getPosition().y * PPM + V_HEIGHT / 4);
+        cam.setPosition(player.getPosition().x * PPM, player.getPosition().y * PPM);
+        cam.update();
 
-        camera.position.set(shipBody.getPosition(), 0);
-        camera.update();
-
-        b2dCam.position.set(shipBody.getPosition(), 0);
+        // Box2d debug.
+//        b2dCam.setPosition(player.getPosition().x + V_WIDTH / 4 / PPM, player.getPosition().y + V_HEIGHT / 4 / PPM);
+        b2dCam.setPosition(player.getPosition().x, player.getPosition().y);
         b2dCam.update();
         debugRenderer.render(world, b2dCam.combined);
 
+        // Game.
+        sb.setProjectionMatrix(cam.combined);
+        player.render(sb);
+        sb.begin();
+        sb.draw(marker1.sprite, marker1.body.getPosition().x * PPM - marker1.sprite.getWidth() / 2, marker1.body.getPosition().y * PPM - marker1.sprite.getHeight() / 2);
+        sb.end();
+
         // HUD.
-        float altitude = shipBody.getPosition().y * PPM - shipSprite.getHeight() / 2;
-        Vector2 velocity = shipBody.getLinearVelocityFromWorldPoint(new Vector2(0f, 0f));
+        sb.setProjectionMatrix(hudCam.combined);
+        float altitude = player.getPosition().y * PPM - player.sprite.getHeight() / 2;
+        Vector2 velocity = player.body.getLinearVelocityFromWorldPoint(new Vector2(0f, 0f));
         hud1.setText("Altitude: " + (int) altitude);
         hud2.setText("Velocity: " + (int) Math.abs(velocity.y * PPM));
+//        hud2.setText("Pos: " + (int) cam.position.x + "/" + (int) cam.position.y);
         stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
         stage.draw();
-
-        // Matches the batch coordinate system to the camera.
-        batch.setProjectionMatrix(camera.combined);
-        batch.begin();
-        shipSprite.draw(batch);
-        batch.draw(groundTexture, 0 * PPM, 0 * PPM, groundTexture.getWidth() * 3, groundTexture.getHeight() * 3, 0, 3, 3, 0);
-        batch.end();
 
         world.step(1/60f, 6, 2);
     }
