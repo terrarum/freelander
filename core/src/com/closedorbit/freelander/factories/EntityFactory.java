8d6bbbff4754c3618d312f4fac5727ac19fde8f5
terrarum <terrarum@gmail.com>
2014-04-30 22:05:27 +0100

    Implemented a texture cache. Case 199

--- core/src/com/closedorbit/freelander/factories/EntityFactory.java
+++ core/src/com/closedorbit/freelander/factories/EntityFactory.java
@@ -11,25 +11,34 @@ import com.badlogic.gdx.physics.box2d.World;
 import com.closedorbit.freelander.entities.RectangleEntity;
 import com.closedorbit.freelander.entities.SpriteEntity;
 
+import java.util.HashMap;
+
 import static com.closedorbit.freelander.utilities.Vars.PPM;
 
 public class EntityFactory {
 
     private World world;
+    private HashMap<String, Texture> textureCache;
+    private Texture texture;
 
     public EntityFactory(World world) {
+        this.textureCache = new HashMap<String, Texture>();
         this.world = world;
     }
 
     public SpriteEntity createSpriteEntity(float x, float y, String imageName) {
-        SpriteEntity entity = new SpriteEntity();
 
-        Texture texture = new Texture(Gdx.files.internal("images/" + imageName + ".png"));
-        int tWidth = texture.getWidth();
-        int tHeight = texture.getHeight();
-        Sprite sprite = new Sprite(texture, 0, 0, tWidth, tHeight);
+        if (!textureCache.containsKey(imageName)) {
+            texture = new Texture(Gdx.files.internal("images/" + imageName + ".png"));
+            textureCache.put(imageName, texture);
+        }
+        else {
+            texture = textureCache.get(imageName);
+        }
+
+        SpriteEntity entity = new SpriteEntity();
+        Sprite sprite = new Sprite(texture, 0, 0, texture.getWidth(), texture.getHeight());
         sprite.setPosition(x, y);
-        texture.dispose();
         entity.sprite = sprite;
         return entity;
     }
