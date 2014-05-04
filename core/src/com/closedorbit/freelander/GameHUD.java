8857db0358cb95bb8bd662fb913a1c6bccd492bb
terrarum <terrarum@gmail.com>
2014-05-04 20:27:13 +0100

    Added horizontal speed indicator to HUD. Case 211

--- core/src/com/closedorbit/freelander/GameHUD.java
+++ core/src/com/closedorbit/freelander/GameHUD.java
@@ -1,8 +1,6 @@
 package com.closedorbit.freelander;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
@@ -11,8 +9,6 @@ import com.badlogic.gdx.scenes.scene2d.utils.Align;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.viewport.ExtendViewport;
 import com.closedorbit.freelander.entities.PlayerEntity;
-import com.closedorbit.freelander.entities.ShipEntity;
-import com.closedorbit.freelander.utilities.FontBuilder;
 import com.closedorbit.freelander.utilities.Vars;
 
 public class GameHUD {
@@ -24,9 +20,9 @@ public class GameHUD {
     private Table table;
 
     private Label altitude;
-    private Label velocity;
-    private Label thrustX;
     private Label thrustY;
+    private Label thrustX;
+    private Label unknown;
     private Image healthbar;
     private Image fuelbar;
 
@@ -49,13 +45,13 @@ public class GameHUD {
 
         // Create labels.
         altitude = new Label("Freelander", skin, "hud-font");
-        velocity = new Label("Freelander", skin, "hud-font");
+        thrustY = new Label("Freelander", skin, "hud-font");
         thrustX = new Label(" 10%", skin, "hud-font");
-        thrustY = new Label(" 10%", skin, "hud-font");
+        unknown = new Label(" 10%", skin, "hud-font");
 
         // Aligns the label text to the right.
         altitude.setAlignment(Align.right);
-        velocity.setAlignment(Align.right);
+        thrustY.setAlignment(Align.right);
 
         int labelPad = 17;
 
@@ -64,7 +60,7 @@ public class GameHUD {
         leftTable.debug();
         // The padding here is to make the top and bottom of the text align with
         // the top and bottom of the table.
-        leftTable.add(thrustY).fillX().expandX().padBottom(labelPad);
+        leftTable.add(unknown).fillX().expandX().padBottom(labelPad);
         leftTable.row();
         leftTable.add(thrustX).fillX().expandX().padTop(labelPad);
 
@@ -73,7 +69,7 @@ public class GameHUD {
         rightTable.debug();
         rightTable.add(altitude).fillX().expandX().padBottom(labelPad);
         rightTable.row();
-        rightTable.add(velocity).fillX().expandX().padTop(labelPad);
+        rightTable.add(thrustY).fillX().expandX().padTop(labelPad);
 
         // Create the health and fuel bars. These are single pixel high bars that are scaled up.
         TextureRegionDrawable healthbarDrawable = new TextureRegionDrawable(new TextureRegion(game.imageCache.getTexture("slider-health")));
@@ -101,14 +97,20 @@ public class GameHUD {
     }
 
     public void update() {
+        // Create direction indicators.
+
+        // Set
+        thrustY.setText(" " + (int) Math.abs(player.getVelocity().y * Vars.PPM) + "m/s ");
+        thrustX.setText(" " + (int) Math.abs(player.getVelocity().x * Vars.PPM) + "m/s ");
+
         altitude.setText((int) player.getAltitude() + "m ");
-        velocity.setText((int) Math.abs(player.getVelocity().y * Vars.PPM) + "m/s ");
 
         currentHealth = player.getHealth() / player.maxHealth * 100;
         currentFuel = player.getFuel() / player.maxFuel * 100;
 
         healthbar.setScaleY(currentHealth);
         fuelbar.setScaleY(currentFuel);
+
     }
 
     public void render(SpriteBatch sb) {
